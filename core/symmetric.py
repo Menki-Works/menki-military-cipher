743f074d1463f7faa25be310b619919c11416f07
Menki-Works <natthaphol.nack.1@gmail.com>
2025-06-25 01:36:12 +0700

    Update symmetric.py

--- core/symmetric.py
+++ core/symmetric.py
@@ -1,1 +1,12 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+import os
 
+def encrypt(data: bytes, key: bytes) -> tuple:
+    aesgcm = AESGCM(key)
+    nonce = os.urandom(12)
+    ct = aesgcm.encrypt(nonce, data, None)
+    return nonce, ct
+
+def decrypt(nonce: bytes, ct: bytes, key: bytes) -> bytes:
+    aesgcm = AESGCM(key)
+    return aesgcm.decrypt(nonce, ct, None)

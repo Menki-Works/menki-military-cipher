3e33b4ab80e7b955dbfa53d0ea308df33a5d9eff
Menki-Works <natthaphol.nack.1@gmail.com>
2025-06-25 01:40:26 +0700

    Update test_full_encrypt.py

--- tests/test_full_encrypt.py
+++ tests/test_full_encrypt.py
@@ -1,1 +1,14 @@
+---
 
+```python
+# Unit test for encryption/decryption
+
+from core.symmetric import encrypt, decrypt
+import os
+
+def test_encrypt_decrypt():
+    key = os.urandom(32)
+    data = b"Secret military-grade text."
+    nonce, ct = encrypt(data, key)
+    pt = decrypt(nonce, ct, key)
+    assert pt == data

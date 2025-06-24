77058fb1e90bdca2ba7e18682868799d4f6ba90a
Menki-Works <natthaphol.nack.1@gmail.com>
2025-06-25 01:36:39 +0700

    Update hash_verify.py

--- core/hash_verify.py
+++ core/hash_verify.py
@@ -1,1 +1,11 @@
+import hmac
+import hashlib
 
+def compute_hmac(key: bytes, data: bytes) -> bytes:
+    return hmac.new(key, data, hashlib.sha3_256).digest()
+
+def verify_hmac(key: bytes, data: bytes, hmac_to_check: bytes) -> bool:
+    return hmac.compare_digest(
+        compute_hmac(key, data),
+        hmac_to_check
+    )
